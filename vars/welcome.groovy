a65199c22d9a64d3d1566c8dd7f741d0f807466b
vemulanaveen44 <75163443+vemulanaveen44@users.noreply.github.com>
2021-07-28 23:05:27 +0530

    added changes

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,52 @@
+/*
 def call(name){
  echo "Hey ${name}, How are you ?"
+}
+*/
+pipeline {
+    agent any
+    tools {
+        terraform 'terraform'
+    }
+    stages {
+        stage('git checkout'){
+            steps {
+                git branch: 'main', credentialsId: 'git-credit', url: 'https://github.com/vemulanaveen44/jennkins-terraform.git'
+            }
+        }
+        stage('init'){
+            steps {
+                ansiColor('xterm') {
+                     sh 'terraform init'
+                }
+                
+            }
+        }
+        stage('plan') {
+            steps {
+                ansiColor('xterm') {
+            
+                    sh 'terraform plan'
+                }
+                
+           }
+        }
+        stage('Approval') {
+      steps {
+        script {
+          def userInput = input(id: 'confirm', message: 'Apply Terraform?', parameters: [ [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Apply terraform', name: 'confirm'] ])
+        }
+      }
+    }
+        
+        stage('apply') {
+            steps {
+                ansiColor('xterm') {
+                    sh 'terraform apply -auto-approve'
+                }
+                
+            }
+        }
+        
+    }
 }
