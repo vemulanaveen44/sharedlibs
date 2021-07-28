f841a32e27cf013347a44b927bca5ff29892cb2a
vemulanaveen44 <75163443+vemulanaveen44@users.noreply.github.com>
2021-07-28 23:59:24 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,28 +1,5 @@
-/*
+
 def call(name){
  echo "Hey ${name}, How are you ?"
 }
-*/
-def call(jenkinsfile){
-    pipeline {
-        agent any
-    
-    }
-    stages {
-        stage('git checkout'){
-            steps {
-                git branch: 'main', credentialsId: 'git-credit', url: 'https://github.com/vemulanaveen44/jennkins-terraform.git'
-            }
-        }
-        stage('init'){
-            setps {
-                ansiColor('xterm'){
-                    sh 'terraform init'
-                    
-                }
-                
-            }
-        }
-    }
-    
-}
+
